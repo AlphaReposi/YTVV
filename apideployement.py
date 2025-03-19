ee20bce376996475ef55189fa7cb6a00957a8937
AlphaReposi <112742109+AlphaReposi@users.noreply.github.com>
2025-03-19 03:24:35 +0100

    Update apideployement.py

--- apideployement.py
+++ apideployement.py
@@ -155,4 +155,5 @@ def api_compute_similarity():
     )
  
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render provides PORT dynamically
+    app.run(host="0.0.0.0", port=port, debug=True)
